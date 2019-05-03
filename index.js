f571aae57fb99a870ce7b78854525e34b31a2628
JP-sDev <jon.pablo80@gmail.com>
2019-05-03 11:41:14 -0400

    init express

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-index.js
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.set("view engine", "ejs");
+
+app.get('/', function(req, res){
+	res.send ('Hello World');
+});
+
+app.listen (3000, function(){
+	console.log("Peau running");
+});
