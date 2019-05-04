5b55d7d040301c32004af3cd203391100fcc5663
Pranjali <pranjaliadhikary@Pranjalis-MacBook-Pro.local>
2019-05-03 22:18:38 -0400

    Just added a line for the register page. Testing git from the terminal

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ var bodyParser = require("body-parser");
 // app.set("view engine", "ejs");
 
 app.get('/', function(req, res){
-	res.send ('Hello World');
+	res.send ('bdhbcfgcu'); //instead of send use render??
 });
 
 app.listen (3000, function(){
 	console.log("Peau running");
+});
+
+//register page
+app.get('/register', function(req, res){
+	res.send ('register page'); //instead of send use render??
 });
